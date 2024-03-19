ddfa16c9eba4418cdb825cc74e5e09e13c19d22c
miguelnunezwb <100383599+miguelnunezwb@users.noreply.github.com>
2024-03-19 15:32:07 -0500

    Update where.tsx
    add address, and maps

--- src/components/section/where.tsx
+++ src/components/section/where.tsx
@@ -14,7 +14,13 @@ export default function Where(props: HTMLProps<HTMLDivElement>) {
         <div className='grid grid-cols-2 gap-6 mt-10'>
           
           <div id='where-main-section'>
-            NetMob 2024 will take place on 7-8 Oct 2024 at the Auditorium of the Worldbank headquarter, located in downtown Washington DC, USA.
+            NetMob 2024 will take place on 7-9 Oct 2024 at the Auditorium of the Worldbank headquarter, located in Washington, DC. U.S.A.
+            <ul className='flex flex-col space-y-1'>
+              <li><span className='font-medium'>Adresse</span>: 1818 H Street, Washington, DC 20433 U.S.A. </li>
+              <li><span className='font-medium'>Worlk Bank</span>: <a href="https://www.worldbank.org/content/dam/meetings/external/springmeeting/SM24%20Campus%20Map-01.jpg">Campus Map</a> </li>
+              <li><span className='font-medium'>Preston Auditorium</span>: <a href="https://www.worldbank.org/content/dam/annualweb/rooms/MC-Preston-01.jpg">Map</a></li>
+              <li><span className='font-medium'>Conference dates</span>: October 7-9, 2024</li>
+            </ul>
           </div>
 
           <div id='worldbank-hq' className='relative bg-white border rounded-xl overflow-hidden shadow-xl w-full h-80'>
