12f260c61243a833e4f03e3d19a9a18ab4d3265f
Vincent Gauthier <pub@luxbulb.org>
2024-03-21 16:24:08 +0100

    add new sponsors logo

--- src/components/section/sponsors.tsx
+++ src/components/section/sponsors.tsx
@@ -6,10 +6,13 @@ export default function Sponsors() {
     <div className="bg-slate-50">
       <div className='pb-20 container mx-auto max-w-screen-lg'>
         <div id="sponsors-title" className='flex justify-center text-6xl italic font-atrament text-red-900'>Sponsors</div>
-        <div id='sponsors-logo' className='pt-10 flex justify-center gap-8'>
+        <div id='sponsors-logo' className='pt-10 flex justify-center items-center gap-8'>
           {/* <Wb className='w-20 h-20' /> */}
+          <Link href="https://www.worldbank.org/en/home"><Image src='/img/sponsors/dime.png' alt='World Bank logo' width={250} height={60} /></Link>
           <Link href="https://www.worldbank.org/en/home"><Image src='/img/sponsors/wb.png' alt='World Bank logo' width={250} height={60} /></Link>
           <Link href="https://www.telecom-sudparis.eu/en/"><Image src='/img/sponsors/tsp.png' alt='World Bank logo' width={140} height={60} /></Link>
+          <Link href="https://www.worldbank.org/en/home"><Image src='/img/sponsors/gdf.webp' alt='World Bank logo' width={250} height={60} /></Link>
+          <Link href="https://www.worldbank.org/en/home"><Image src='/img/sponsors/maece.webp' alt='World Bank logo' width={300} height={60} /></Link>
         </div>
       </div>
     </div>
