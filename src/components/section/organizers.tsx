822a0aa776df709c1959d5669c5fd491bf7687b2
Vincent Gauthier <pub@luxbulb.org>
2024-04-02 08:57:51 +0200

    add sveta

--- src/components/section/organizers.tsx
+++ src/components/section/organizers.tsx
@@ -5,16 +5,33 @@ import Link from 'next/link'
 export default function Organizers() {
   return (
     <div id="steering_committee" className='bg-slate-50'>
-      <div className='pt-20 container mx-auto max-w-xs sm:max-w-xl md:max-w-screen-sm xl:max-w-screen-lg'>
+      <div className='pt-20 pb-20 container mx-auto max-w-xs sm:max-w-xl md:max-w-screen-sm xl:max-w-screen-lg'>
         <div className='flex justify-center text-5xl lg:text-8xl italic font-atrament text-red-900'>Local Organizers</div>
-        
+
         <div className='mt-20 grid grid-cols-2 lg:grid-cols-12 gap-12 justify-center'>
-        <div className='lg:col-span-3 flex flex-col justify-top items-center'>
+          {/* Organizers: Miguel */}
+          <div className='lg:col-span-3 flex flex-col justify-top items-center'>
             <div className="w-32 h-32 mb-5 relative">
               <Image
                 className="rounded-full border border-gray-100 shadow-lg"
                 src="/img/organisers/miguel.webp"
-                alt="Vincent Blondel"
+                alt="Miguel Nuñez del Prado Cortez"
+                object-fit="cover"
+                fill={true}
+              />
+            </div>
+            <Link href="https://perso.uclouvain.be/vincent.blondel/" className='underline text-center decoration-red-400'>Miguel Nuñez del Prado Cortez</Link>
+            <p className='text-center'>World Bank</p>
+          </div>
+
+
+          {/* Organizers: Sveta */}
+          <div className='lg:col-span-3 flex flex-col justify-top items-center'>
+            <div className="w-32 h-32 mb-5 relative">
+              <Image
+                className="rounded-full border border-gray-100 shadow-lg"
+                src="/img/organisers/sveta.webp"
+                alt="Miguel Nuñez del Prado Cortez"
                 object-fit="cover"
                 fill={true}
               />
