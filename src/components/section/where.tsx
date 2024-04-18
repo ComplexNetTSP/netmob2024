29b2930ab0507b1e207212fae9bcc2db2bdc0474
Vincent Gauthier <pub@luxbulb.org>
2024-04-19 00:12:20 +0200

    fix some the issue #15

--- src/components/section/where.tsx
+++ src/components/section/where.tsx
@@ -19,14 +19,14 @@ export default function Where(props: HTMLProps<HTMLDivElement>) {
               NetMob 2024 will take place on 7-9 Oct 2024 at the <Link className='hover:underline decoration-red-400' href="https://www.worldbank.org/content/dam/annualweb/rooms/MC-Preston-01.jpg">Preston Auditorium</Link> located inside <Link className='hover:underline decoration-red-400' href="https://www.worldbank.org/content/dam/meetings/external/springmeeting/SM24%20Campus%20Map-01.jpg">The World Bank Group Main Campus</Link> in Washington, DC. U.S.A.
             </p>
             <div className='flex flex-col mt-5 space-y-3'>
-              <div><span className='font-medium'>The World Bank Group Main Campus visitor entrance</span>: <Link className='hover:underline decoration-red-400' href="https://www.google.fr/maps/place/752+18th+St+NW,+Washington,+DC+20006,+%C3%89tats-Unis/@38.899122,-77.0430488,19z/data=!3m1!4b1!4m6!3m5!1s0x89b7b7b220d64815:0xf49c452c52ca58f4!8m2!3d38.899121!4d-77.0418324!16s%2Fg%2F11y1f_426r?entry=ttu">752 18th St NW, Washington, DC 20006, USA</Link></div>
+              <div><span className='font-medium'>The World Bank Group Main Campus visitor entrance</span>: <Link href="https://www.google.fr/maps/place/752+18th+St+NW,+Washington,+DC+20006,+%C3%89tats-Unis/@38.899122,-77.0430488,19z/data=!3m1!4b1!4m6!3m5!1s0x89b7b7b220d64815:0xf49c452c52ca58f4!8m2!3d38.899121!4d-77.0418324!16s%2Fg%2F11y1f_426r?entry=ttu">752 18th St NW, Washington, DC 20006, USA</Link></div>
               <div><span className='font-medium'>Conference dates</span>: October 7-9, 2024</div>
               <div>
                 <span className='font-medium'>Nearby metro stations</span>: 
                 <ul className="list-disc ml-4">
-                  <li><Link className='hover:underline decoration-red-400' href="https://washington.org/navigating-dc-metro">Farragut West (the blue, orange and silver line)</Link>
+                  <li><Link href="https://washington.org/navigating-dc-metro">Farragut West (the blue, orange and silver line)</Link>
                   </li>
-                  <li><Link className='hover:underline decoration-red-400' href="https://washington.org/navigating-dc-metro"> Farragut North (Red Line)</Link>
+                  <li><Link href="https://washington.org/navigating-dc-metro"> Farragut North (Red Line)</Link>
                   </li>
                 </ul>
               </div>
