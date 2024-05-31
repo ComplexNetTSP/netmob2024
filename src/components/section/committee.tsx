788c0f5b55d489676c99d1c9b1a998bfaf619b70
Vincent Gauthier <pub@luxbulb.org>
2024-05-31 09:30:52 +0200

    update committee

--- src/components/section/committee.tsx
+++ src/components/section/committee.tsx
@@ -27,6 +27,7 @@ export default function Committee() {
     { name: "Veena Mendiratta", affiliation: "Northwestern University, US", url: "https://www.mccormick.northwestern.edu/research-faculty/directory/affiliated/mendiratta-veena.html"},
     { name: "Hugo Alatrista-Salas", affiliation: "De Vinci University, France", url: "http://hugo.alatristasalas.free.fr/Alatsal/Welcome.html"},
     { name: "Rosario Medina", affiliation: "Pontifical Catholic University, Peru", url: "https://rmedinar.github.io/"},
+    { name: "Yang Yue", affiliation: "Shenzhen University, China", url: "https://scholar.google.com/citations?user=Or3pqN8AAAAJ&hl=en"},
   ] 
 
   function CommitteeMembers() {
@@ -47,28 +48,6 @@ export default function Committee() {
         <div className='flex justify-center text-5xl lg:text-8xl font-atrament italic text-red-900'>Program Committee</div>
         <div className='pt-16 grid grid-cols-1 lg:grid-cols-2 lg:gap-x-8 gap-y-2'>
           <CommitteeMembers />
-        {/*
-        <div>Albert Ali Salah, Utrecht University, The Netherlands</div>
-        <div>Bruno Lepri, FBK Foundation, Italy</div>
-        <div>Carlee Joe-Wong, Carnegie Mellon University, US</div>
-        <div>Enrique Frias-Martinez, Universidad Camilo Jose Cela, Spain</div>
-        <div>Giovanna Miritello, Vodafone, UK</div>
-        <div>Giulia Pullano, Georgetown University, US</div>
-        <div>János Kertész, Central European University, Vienna</div>
-        <div>Katarzyna Wac, University of Geneva, Switzerland</div>
-        <div>Luca Pappalardo, ISTI-CNR, Italy</div>
-        <div>Luca Vassio, Politecnico di Torino, Italy</div>
-        <div>Marta Gonzalez, Berkeley, US</div>
-        <div>Mirco Musolesi, University College London, UK</div>
-        <div>Mori Korokawa, KDDI Research, Japan</div>
-        <div>Ruben Cuevas Rumin, Universidad Carlos III de Madrid, Spain</div>
-        <div>Stefania Rubrichi, Orange Innovation, France</div>
-        <div>Takahiro Yabe, NYU, US</div>
-        <div>Tassos Noulas, Firefly, US</div>
-        <div>Veena Mendiratta, Northwestern University, US</div>
-        <div>Yoshihide Sekimoto, University of Tokio, Japan</div>
-        <div>Zbigniew Smoreda, Orange Innovation, France</div>
-        */}
         </div>
       </div>
     </div>
