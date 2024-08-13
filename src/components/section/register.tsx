befe4e4c2fb696b5055dd520379e7c5fa85601b3
Vincent Gauthier <pub@luxbulb.org>
2024-08-13 13:15:22 +0200

    update the register table add responsive design

--- src/components/section/register.tsx
+++ src/components/section/register.tsx
@@ -10,10 +10,11 @@ export default function Register(props: HTMLProps<HTMLDivElement>) {
       <div className='pt-20 pb-10 container mx-auto max-w-xs sm:max-w-xl md:max-w-screen-sm xl:max-w-screen-lg'>
         { /* Title */}
         <div id="register-title" className='flex justify-center text-5xl lg:text-8xl italic font-atrament text-red-900'>Registration fees</div>
+      </div>
 
         { /* table  */}
-        <div id='register-table' className='mt-10 flex justify-center rounded-xl shadow-xl border bg-slate-100'>
-          <table className="table-auto w-full my-5 mx-20 border-separate border-spacing-y-3">
+        <div id='register-table' className='mt-10 px-4 mx-auto max-w-xs sm:max-w-xl flex justify-center rounded-xl shadow-xl border bg-slate-100'>
+          <table className="w-full mt-4 border-separate border-spacing-y-3">
             <thead>
               <tr>
                 <th className='text-left border-b border-slate-300'>Attendee</th>
@@ -27,29 +28,30 @@ export default function Register(props: HTMLProps<HTMLDivElement>) {
                 <td><Badge>Student</Badge></td>
                 <td>$ 125</td>
                 <td>$ 175</td>
-                <td className='flex justify-center'><Link href="https://web.cvent.com/event/c0d352e4-b919-4595-9f15-5ee760caf3de/regProcessStep1"><button className='px-4 py-1 bg-green-600 border border-green-700 text-white uppercase rounded-lg'>register</button></Link></td>
+                <td className='flex justify-center'><Link href="https://web.cvent.com/event/c0d352e4-b919-4595-9f15-5ee760caf3de/regProcessStep1"><button className='px-2 py-1 bg-green-600 border border-green-700 text-white rounded-lg'>register</button></Link></td>
               </tr>
               <tr className='mt-16'>
                 <td><Badge>Academic</Badge></td>
                 <td>$ 250</td>
                 <td>$ 350</td>
-                <td className='flex justify-center'><Link href="https://web.cvent.com/event/c0d352e4-b919-4595-9f15-5ee760caf3de/regProcessStep1"><button className='px-4 py-1 bg-green-600 border border-green-700 text-white uppercase rounded-lg'>register</button></Link></td>
+                <td className='flex justify-center'><Link href="https://web.cvent.com/event/c0d352e4-b919-4595-9f15-5ee760caf3de/regProcessStep1"><button className='px-2 py-1 bg-green-600 border border-green-700 text-white rounded-lg'>register</button></Link></td>
               </tr>
               <tr className='mt-16'>
                 <td><Badge>Industry</Badge></td>
                 <td>$ 250</td>
                 <td>$ 350</td>
-                <td className='flex justify-center'><Link href="https://web.cvent.com/event/c0d352e4-b919-4595-9f15-5ee760caf3de/regProcessStep1"><button className='px-4 py-1 bg-green-600 border border-green-700 text-white uppercase rounded-lg'>register</button></Link></td>
+                <td className='flex justify-center'><Link href="https://web.cvent.com/event/c0d352e4-b919-4595-9f15-5ee760caf3de/regProcessStep1"><button className='px-2 py-1 bg-green-600 border border-green-700 text-white rounded-lg'>register</button></Link></td>
               </tr>
             </tbody>
-            <caption className="mt-10 caption-bottom">
+            <caption className="mt-10 mb-5 caption-bottom">
               *Early Bird ends on August 30, 2024. Regular registration ends on October 7, 2024.
             </caption>
           </table>
         </div>
 
+      <div className='pt-20 pb-10 container mx-auto max-w-xs sm:max-w-xl md:max-w-screen-sm xl:max-w-screen-lg'>
         { /* refound */}
-        <div id='refound' className='pt-10 flex text-4xl font-atrament'>Refund/Cancellation Policy</div>
+        <div id='refound' className='pt-10 flex text-4xl font-atrament'>Cancellation Policy</div>
         
         <div className='pt-5 text-justify'>
         All registration refund requests must be sent via email to <Link href="mailto:netmob2024@worldbank.org" className='font-semibold text-red-900'>netmob2024@worldbank.org</Link> and received no later than September 27, 2024. A full refund, minus 25.00 USD for online registration, will be issued. For those who pay by credit card, refunds can only be processed to the credit card to which the original charge was made. Processing of the refunds may take over 30 days. Refund requests received after September 27, 2024 will not be honored. No-shows are subject to the full fee.
